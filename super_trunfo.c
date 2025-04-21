34e6ac752f0eeab9970641839da7a051e3024aba
roonald7 <roonald.anderson017@gmail.com>
2025-04-21 09:44:38 -0300

    Desafio nível novato concluído

--- super_trunfo.c
+++ super_trunfo.c
@@ -17,17 +17,6 @@ typedef struct Carta {
 } Carta;
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     Carta carta1, carta2;
 
     // Inserindo os dados da primeira carta
